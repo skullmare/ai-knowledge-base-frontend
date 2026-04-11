219a94c6b9afe3cca0fab5a47136576858d59efd
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-04-12 01:47:03 +0300

    add log vite_api_url

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AppRouter from '@router/App';
 import ErrorSnackbarStack from '@ui/Snackbar/ErrorSnackbarStack'
 
 function App() {
+  console.log('API URL:', import.meta.env.VITE_API_URL)
   const fetchProfile = useProfileStore((state) => state.fetchProfile);
   const accessToken = localStorage.getItem('accessToken');
   useEffect(() => {
